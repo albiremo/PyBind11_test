7f6ca20fe5ce011e98c1f3a1bc837658aeac9a20
albiremo <albertoremigi92@gmail.com>
2021-09-08 14:18:22 +0200

    interface for references created

--- main.cpp
+++ main.cpp
@@ -1,11 +1,33 @@
+using namespace std;
+#include <vector>
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 namespace py = pybind11;
 
 float some_fn(float arg1, float arg2){
  return arg1+arg2;
 }
 
+void test(//long (&sizes)[1],
+         const vector<long> &adjMatrix_row_ptr,
+         const vector<long> &adjMatrix_col_ind)
+{
+  for (int i = 0; i< adjMatrix_row_ptr.size(); i++)
+  {
+    cout << "m. single parents = " << adjMatrix_row_ptr[i] << endl;
+  }
+}
+
 PYBIND11_MODULE(module_name, module_handle) {
   module_handle.doc() = "I'm a docstring hehe";
   module_handle.def("some_fn_python_name", &some_fn);
+  module_handle.def("test",
+  [](const vector<long> adjMatrix_row_ptr,const vector<long> adjMatrix_col_ind)
+      {
+          test(adjMatrix_row_ptr,adjMatrix_col_ind);
+      });
+
+
+//  module_handle.def("some_fn_python_name", &test);
+
 }
