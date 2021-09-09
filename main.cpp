09424363784478eda1623f57f9d14df9d02f83a1
albiremo <albertoremigi92@gmail.com>
2021-09-09 14:35:12 +0200

    modified reference in python Interface

--- main.cpp
+++ main.cpp
@@ -9,11 +9,12 @@ float some_fn(float arg1, float arg2){
 }
 
 void test(//long (&sizes)[1],
-         const vector<long> &adjMatrix_row_ptr,
+         vector<long> &adjMatrix_row_ptr,
          const vector<long> &adjMatrix_col_ind)
 {
   for (int i = 0; i< adjMatrix_row_ptr.size(); i++)
   {
+    adjMatrix_row_ptr[i]=5;     
     cout << "m. single parents = " << adjMatrix_row_ptr[i] << endl;
   }
 }
@@ -22,9 +23,10 @@ PYBIND11_MODULE(module_name, module_handle) {
   module_handle.doc() = "I'm a docstring hehe";
   module_handle.def("some_fn_python_name", &some_fn);
   module_handle.def("test",
-  [](const vector<long> adjMatrix_row_ptr,const vector<long> adjMatrix_col_ind)
+  [](vector<long> adjMatrix_row_ptr,const vector<long> adjMatrix_col_ind)
       {
           test(adjMatrix_row_ptr,adjMatrix_col_ind);
+          return std::make_tuple(adjMatrix_row_ptr);
       });
 
 
